1e6f882601875e14a19df592dcbb6c174e6d4fad
Michael Haurholm Sternbo <mich475r@edu.easj.dk>
2016-10-25 13:20:05 +0200

    returns 234 1kroner, arbejd videre! :P

--- Sandbox/InsertCodeHere.cs
+++ Sandbox/InsertCodeHere.cs
@@ -17,15 +17,18 @@ namespace Sandbox
             // To keep things simple, we assume that you pay back
             // change as: 1-kr coins, 10-kr coins and 100-kr bills.
 
-            int noOf1krCoins = 0;
-            int noOf10krCoins = 0;
-            int noOf100krBills = 0;
+            int noOf1krCoins = 1;
+            int noOf10krCoins = 10;
+            int noOf100krBills = 100;
 
-
+            int _noOf1krCoins = (int)(changeToPayBack / noOf1krCoins);
+            int _noOf10krCoins = (int)((changeToPayBack % noOf1krCoins) / noOf10krCoins);
+            int _noOf100krBills = (int)(((changeToPayBack % noOf1krCoins) & noOf10krCoins) / noOf100krBills);
+            
             // YOUR JOB: Add code to calculate the correct change
 
 
-            Console.WriteLine("Your change is {0} 1-kr coins, {1} 10-kr coins and {2} 100-kr bills", noOf1krCoins, noOf10krCoins, noOf100krBills);
+            Console.WriteLine("Your change is {0} 1-kr coins, {1} 10-kr coins and {2} 100-kr bills", _noOf1krCoins, _noOf10krCoins, _noOf100krBills);
             Console.WriteLine("A total of {0} kr", changeToPayBack);
 
 
